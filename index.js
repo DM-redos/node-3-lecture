e85531a2b746a1a8f1a155515cf14602ec265fe8
Jonathan May <jonathan.may@devmountain.com>
2018-12-18 07:52:27 -0600

    Add solution

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const express = require("express");
 const { json } = require("body-parser");
+const { logger } = require("./middlewares");
 const app = express();
 const numbers = [12, 4, 63, 19, 48];
 
-app.use(json());
+app.use(json(logger));
 
 // We need to know the date and time for every request.
+app.use(logger);
 
 app.get("/api/numbers", (req, res, next) => {
   res.json(numbers);
 });
 
 // The routes below should be protected.
+app.use((req, res, next) => {
+  if (req.body.user === "admin") {
+    next();
+  } else {
+    res.json({ error: "Unauthorized" });
+  }
+});
 app.post("/api/numbers", (req, res, next) => {
   numbers.push(req.body.number);
   res.json(numbers);
 });
-app.put("/api/numbers/:id", (req, res, next) => {
-  numbers.splice(req.params.id, 1, req.body.number);
-  res.json(numbers);
-});
+app.put(
+  "/api/numbers/:id",
+
+  (req, res, next) => {
+    numbers.splice(req.params.id, 1, req.body.number);
+    res.json(numbers);
+  }
+);
 
 // There's a bug here, let's use middleware to find it
-app.delete("/api/numbers/:id", (req, res, next) => {
-  numbers.splice(req.body.id, 1);
-  res.json(numbers);
-});
+app.delete(
+  "/api/numbers/:id",
+  (req, res, next) => {
+    console.log("Endpoint hit");
+    console.log(req.body);
+    next();
+  },
+  (req, res, next) => {
+    numbers.splice(req.body.id, 1);
+    res.json(numbers);
+  }
+);
 
 app.listen(5050, () => "Listening on 5050");
---
+++ middlewares.js
@@ -0,0 +1,6 @@
+const logger = (req, res, next) => {
+  console.log(new Date());
+  next();
+};
+
+module.exports = { logger };
