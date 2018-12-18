f23530e777ab0442d103d77b6b18e22e37a59bd0
Li Swafford <liswafford@yahoo.com>
2018-12-18 12:05:25 -0600

    new

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const { json } = require("body-parser");
 const { logger } = require("./middlewares");
+const session = require('express-session');
 const app = express();
 const numbers = [12, 4, 63, 19, 48];
 
+app.use(session(
+  { secret: "house hunters intl",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 60 * 24 * 7
+    }
+  }
+))
 app.use(json());
 
 // We need to know the date and time for every request.
@@ -11,11 +21,15 @@ app.use(logger);
 
 app.get("/api/numbers", (req, res, next) => {
   res.json(numbers);
+  req.session.user = "LI";
+  console.log("get:", req.session);
 });
 
 // The routes below should be protected.
 app.use((req, res, next) => {
-  if (req.body.user === "admin") {
+  if (req.body.username === "admin") {
+    console.log("middle:", req.session);
+    req.session.test = 'test'
     next();
   } else {
     res.json({ error: "Unauthorized" });
@@ -23,6 +37,7 @@ app.use((req, res, next) => {
 });
 app.post("/api/numbers", (req, res, next) => {
   numbers.push(req.body.number);
+  console.log("post:", req.session)
   res.json(numbers);
 });
 app.put(
